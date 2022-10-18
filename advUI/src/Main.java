bbdf726ba0978291c3cf8deddd20274b42ba008a
nnhanus <noemie.hanus@laposte.net>
2022-10-18 22:31:33 +0200

    put font and colors in Main, added comments and documentation to MainMenu

--- advUI/src/Main.java
+++ advUI/src/Main.java
@@ -1,4 +1,29 @@
+import java.awt.*;
+
 public class Main {
+    public static String fontName = "Bradley Hand";
+    public static Color bgColor = new Color(0xFED1FF);
+    public static Color green = new Color(0,153,0);
+    public static Color red = new Color(204, 0, 0);
+
+    /**
+     * Returns the game font in bold in given size
+     * @param size the size of the font
+     * @return the font with the right size
+     */
+    public static Font getFontBold(int size){
+        return new Font(fontName, Font.BOLD, size);
+    }
+
+    /**
+     * Returns the game font in given size
+     * @param size the size of the font
+     * @return the font with the right size
+     */
+    public static Font getFontPlain(int size){
+        return new Font(fontName, Font.PLAIN, size);
+    }
+
     public static void main(String[] args) {
         MainMenu menu = new MainMenu();
     }
--- advUI/src/MainMenu.java
+++ advUI/src/MainMenu.java
@@ -12,12 +12,13 @@ public class MainMenu extends JDialog {
 
     MainMenuPresentation view;
     String mode = "slider"; //toggle between level select (=slider) and character select (=chooser)
-    String characterFile = "advUI/Icons/cow.png";
+    String characterFile = "advUI/Icons/cow.png"; //default character
     JLabel character;
     boolean tutorialOn=true;
 
     /**
-     * Constructor for game launch
+     * Constructor
+     * Character automatically set to Cow, unlocked levels set to 1
      */
     public MainMenu(){
         super();
@@ -28,6 +29,11 @@ public class MainMenu extends JDialog {
         pack();
     }
 
+    /**
+     * Constructor
+     * @param levelUnlocked current unlocked levels
+     * @param character current character
+     */
     public MainMenu(int levelUnlocked, String character){
         super();
         this.setDefaultCloseOperation( WindowConstants.DISPOSE_ON_CLOSE );
@@ -39,126 +45,95 @@ public class MainMenu extends JDialog {
     }
 
 
+    /**
+     * Class to handle the presentation of the MainMenu
+     */
     private class MainMenuPresentation {
-
         MainMenu control;
         int unlocked;
+
         public MainMenuPresentation(MainMenu control, int levelUnlocked){
             this.control=control;
-            Color BGColor = new Color(0xFED1FF);
             this.control.setPreferredSize(new Dimension(500, 500));
-            this.control.setBackground(BGColor);
+            this.control.setBackground(Main.bgColor);
             this.control.setLayout(new BorderLayout());
             unlocked = levelUnlocked;
 
-            mainTitle(BGColor);
-
-            mainPane(BGColor);
+            mainTitle();
+            mainPane();
         }
-        private void mainPane(Color BGColor) {
+
+        /**
+         * Handles the creation and layout of the main pane
+         */
+        private void mainPane() {
             JPanel mainPane = new JPanel();
             mainPane.setPreferredSize(new Dimension(GameWindowModel.Width, GameWindowModel.Height));
-            mainPane.setBackground(BGColor);
+            mainPane.setBackground(Main.bgColor);
             mainPane.setLayout(new BorderLayout());
             mainPane.setBorder(new EmptyBorder(new Insets(10, 10, 10, 10)));
 
+            //Panel with the level select and the character select
             JPanel levelSelect = new JPanel();
             levelSelect.setLayout(new BorderLayout());
 
+            //character select and level select
             CharacterChooser choose = new CharacterChooser();
             SRSlider slider = new SRSlider(1, unlocked);
             levelSelect.add(slider, BorderLayout.CENTER);
 
+            //toggle tutorials
             JLabel tutorial = new JLabel("Tutorials On");
-            tutorial.setFont(new Font("Bradley Hand", Font.PLAIN, 18));
-            tutorial.setForeground(new Color(0,153,0));
+            tutorial.setFont(Main.getFontPlain(18));
+            tutorial.setForeground(Main.green);
             tutorial.setOpaque(false);
             tutorial.addMouseListener(new MouseAdapter() {
                 @Override
                 public void mouseClicked(MouseEvent e) {
                     if (tutorialOn) {
                         tutorial.setText("Tutorials Off");
-                        tutorial.setForeground(new Color(204, 0, 0));
+                        tutorial.setForeground(Main.red);
                         tutorialOn = false;
                     } else {
                         tutorial.setText("Tutorials On");
-                        tutorial.setForeground(new Color(0, 153, 15));
+                        tutorial.setForeground(Main.green);
                         tutorialOn = true;
                     }
                 }
                 @Override
                 public void mouseEntered(MouseEvent e){
-                    tutorial.setFont(new Font("Bradley Hand", Font.PLAIN, 20));
+                    tutorial.setFont(Main.getFontPlain(20));
                 }
                 @Override
                 public void mouseExited(MouseEvent e){
-                    tutorial.setFont(new Font("Bradley Hand", Font.PLAIN, 18));
+                    tutorial.setFont(Main.getFontPlain(18));
                 }
             });
 
+            //Level label
             JLabel level = new JLabel("Choose your level!", SwingConstants.CENTER);
-            level.setFont(new Font("Bradley Hand", Font.BOLD, 30));
-            level.setBackground(BGColor);
+            level.setFont(Main.getFontBold(30));
+            level.setBackground(Main.bgColor);
             level.setOpaque(true);
             levelSelect.add(level, BorderLayout.NORTH);
 
             JPanel playPanel = new JPanel();
             playPanel.setLayout(new BoxLayout(playPanel, BoxLayout.LINE_AXIS));
-            playPanel.setBackground(BGColor);
+            playPanel.setBackground(Main.bgColor);
+
+            //Go button
             JButton go = new JButton("Go");
-            go.setFont(new Font("Bradley Hand", Font.BOLD, 24));
-            go.setBackground(BGColor);
-            go.addActionListener(
-                    e -> {
-                        if (mode.equalsIgnoreCase("slider")) {
-                            int selectedLevel = slider.getValue();
-                            if (selectedLevel == 0) {
-                            } else if (selectedLevel == 6) {
-                                dispose();
-                                System.exit(0);
-                            } else {
-                                GameWindow game = new GameWindow(slider.getValue(), tutorialOn, characterFile, unlocked);
-                                dispose();
-                            }
-                        } else {
-                            characterFile = choose.characters.get(choose.index);
-                            character.setIcon(new ImageIcon(new ImageIcon(characterFile).getImage().getScaledInstance(50,50,Image.SCALE_DEFAULT)));
-                            levelSelect.remove(choose);
-                            levelSelect.add(slider, BorderLayout.CENTER);
-                            playPanel.removeAll();
-                            playPanel.add(character);
-                            playPanel.add(Box.createRigidArea(new Dimension(140,0)));
-                            playPanel.add(go);
-                            playPanel.add(Box.createRigidArea(new Dimension(100,0)));
-                            playPanel.add(tutorial);
-                            //character.setText("Character");
-                            mode = "slider";
-                            level.setText("Choose your level!");
-                            revalidate();
-                            repaint();
+            go.setFont(Main.getFontBold(25));
+            go.setBackground(Main.bgColor);
+            go.addActionListener(e -> goButtonListener(levelSelect, choose, slider, tutorial, level, playPanel, go));
 
-                        }
-                    }
-            );
-
-
+            //Character icon at bottom, clicking it goes to character select
             character = new JLabel(new ImageIcon(new ImageIcon(characterFile).getImage().getScaledInstance(50,50,Image.SCALE_DEFAULT)));
             character.setOpaque(false);
             character.addMouseListener(new MouseAdapter() {
                 @Override
                 public void mouseClicked(MouseEvent e) {
-                    if (mode.equalsIgnoreCase("slider")) {
-                        levelSelect.remove(slider);
-                        levelSelect.add(choose, BorderLayout.CENTER);
-                        playPanel.removeAll();
-                        playPanel.add(Box.createHorizontalGlue());
-                        playPanel.add(go);
-                        playPanel.add(Box.createHorizontalGlue());
-                        mode = "chooser";
-                        level.setText("Choose your character!");
-                        revalidate();
-                        repaint();
-                    }
+                    characterMouseClicked(levelSelect, slider, choose, playPanel, go, level);
                 }
                 @Override
                 public void mouseEntered(MouseEvent e){
@@ -169,12 +144,13 @@ public class MainMenu extends JDialog {
                     character.setIcon(new ImageIcon(new ImageIcon(characterFile).getImage().getScaledInstance(50,50,Image.SCALE_DEFAULT)));
                 }
             });
+
+
             playPanel.add(character);
             playPanel.add(Box.createRigidArea(new Dimension(140,0)));
             playPanel.add(go);
             playPanel.add(Box.createRigidArea(new Dimension(100,0)));
             playPanel.add(tutorial);
-
 
 
             levelSelect.add(playPanel, BorderLayout.PAGE_END);
@@ -184,20 +160,78 @@ public class MainMenu extends JDialog {
             control.add(mainPane, BorderLayout.CENTER);
         }
 
+        /**
+         * Handle MouseClicked event for character icon
+         * Move to chooser mode
+         * Removes the slider and display the character chooser
+         */
+        private void characterMouseClicked(JPanel levelSelect, SRSlider slider, CharacterChooser choose, JPanel playPanel, JButton go, JLabel level) {
+            if (mode.equalsIgnoreCase("slider")) {
+                levelSelect.remove(slider);
+                levelSelect.add(choose, BorderLayout.CENTER);
+                playPanel.removeAll();
+                playPanel.add(Box.createHorizontalGlue());
+                playPanel.add(go);
+                playPanel.add(Box.createHorizontalGlue());
+                mode = "chooser";
+                level.setText("Choose your character!");
+                revalidate();
+                repaint();
+            }
+        }
 
-        private void mainTitle(Color BGColor) {
+        /**
+         *Handles the ActionListener for the go button
+         * if in slider mode, start the game
+         * if in chooser mode, goes back to slider mode
+         */
+        private void goButtonListener(JPanel levelSelect, CharacterChooser choose, SRSlider slider, JLabel tutorial, JLabel level, JPanel playPanel, JButton go) {
+            if (mode.equalsIgnoreCase("slider")) { //start the game
+                int selectedLevel = slider.getValue();
+                if (selectedLevel == 0) {
+                } else if (selectedLevel == 6) {
+                    dispose();
+                    System.exit(0);
+                } else {
+                    GameWindow game = new GameWindow(slider.getValue(), tutorialOn, characterFile, unlocked);
+                    dispose();
+                }
+            } else { //goes back to level select
+                characterFile = choose.characters.get(choose.index);
+                character.setIcon(new ImageIcon(new ImageIcon(characterFile).getImage().getScaledInstance(50,50,Image.SCALE_DEFAULT)));
+                levelSelect.remove(choose);
+                levelSelect.add(slider, BorderLayout.CENTER);
+                playPanel.removeAll();
+                playPanel.add(character);
+                playPanel.add(Box.createRigidArea(new Dimension(140,0)));
+                playPanel.add(go);
+                playPanel.add(Box.createRigidArea(new Dimension(100,0)));
+                playPanel.add(tutorial);
+                mode = "slider";
+                level.setText("Choose your level!");
+                revalidate();
+                repaint();
+
+            }
+        }
+
+
+        /**
+         * Handle the layout of the main title
+         */
+        private void mainTitle() {
             JPanel mainTitle = new JPanel();
             mainTitle.setLayout(new BorderLayout());
 
             JLabel welcome = new JLabel("WELCOME TO", SwingConstants.CENTER);
-            welcome.setFont(new Font("Bradley Hand", Font.PLAIN, 35));
+            welcome.setFont(Main.getFontPlain(35));
             welcome.setOpaque(true);
-            welcome.setBackground(BGColor);
+            welcome.setBackground(Main.bgColor);
             mainTitle.add(welcome, BorderLayout.NORTH);
 
             JLabel title = new JLabel("SCOOP RECOUP!", SwingConstants.CENTER);
-            title.setFont(new Font("Bradley Hand", Font.PLAIN, 50));
-            title.setBackground(BGColor);
+            title.setFont(Main.getFontPlain(50));
+            title.setBackground(Main.bgColor);
             title.setOpaque(true);
             mainTitle.add(title, BorderLayout.CENTER);
 
